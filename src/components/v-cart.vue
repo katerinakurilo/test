b0aebe1ff278fd99f7d31c015d1c249e278abeaa
katerinakurilo <katerina_vli@mail.ru>
2020-12-04 15:47:49 +0500

    Созданы и импортированы компоненты

---
+++ src/components/v-cart.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="v-cart">
+<p>Cart</p>
+</div>
+
+</template>
+
+
+
+
+<script>
+export default {
+    name: "v-cart",
+    props: {},
+    data() {
+        return {
+
+        }
+    }
+
+}
+
+
+</script>
+
+
+
+
+<style></style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="v-main-wrapper">
+<p>{{title}}</p>
+<v-catalog/>
+<v-cart/>
+</div>
+</template>
+
+
+
+
+<script>
+
+import vCatalog from './v-catalog'
+import vCart from './v-cart'
+export default {
+    name: 'v-main-wrapper',
+    components: {
+        vCatalog,
+        vCart
+    },
+    props: {},
+    data() {
+        return {
+            title: 'Main wrapper'
+        }
+    }
+}
+</script>
+
+
+
+
+<style>
+
+</style>
